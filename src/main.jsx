4a8a1ec04e204acf5542b4668a9d658505f47a7b
euluis-dev <fluisf00@gmail.com>
2024-04-15 19:11:54 -0300

    imported pages in main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Movie from './pages/Movie.jsx';
+import Search from './pages/Search.jsx'; 
 
 import './index.css';
 
