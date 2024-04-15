4855fe23901751d32b9de6ceaa515e1b73826bbf
euluis-dev <fluisf00@gmail.com>
2024-04-15 18:55:30 -0300

    was imported BrowserRouter, Routes, Route from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App.jsx';
+
+import './index.css';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
