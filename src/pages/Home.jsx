351c00c5f0f7808e5c5dc73f23ce0820aa41804c
euluis-dev <fluisf00@gmail.com>
2024-04-17 19:56:03 -0300

    teh getTopRatedMovies function was created to handle the API query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,14 @@ const moviesURL = import.meta.env.VITE_API  // import variable of the .env
 const apiKey = import.meta.env.VITE_API_KEY  // import variable of the .env
 
 const Home = () => {
-    console.log(moviesURL, apiKey);
+    const [topMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await json(res);
+
+        console.log(data);
+    }
 
     return (
         <div>
