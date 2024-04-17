6001bdfdaa75e203fbfdb61d06aafb857e1f39fe
euluis-dev <fluisf00@gmail.com>
2024-04-17 19:32:07 -0300

    import variable of the .env in the Home.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API  // import variable of the .env
+const apiKey = import.meta.env.VITE_API_KEY  // import variable of the .env
+
 const Home = () => {
+    console.log(moviesURL, apiKey);
+
     return (
         <div>
             <h2>Home</h2>
