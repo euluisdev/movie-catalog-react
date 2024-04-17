3e1cc6a85702c8c4f4355509031a2b21b1b027fe
euluis-dev <fluisf00@gmail.com>
2024-04-17 12:22:08 -0300

    icons from the react-icons-Bi lib werw imported

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 const NavBar = () => {
 
     return (
         <nav id="navbar">
             <h2>
-                <Link to='/'>MoviesCatalog</Link>
+                <Link to='/'>
+                    <BiCameraMovie /> MoviesCatalog
+                </Link>
             </h2>
             <form>
                 <input type="text" placeholder="Busque seu filme" />
-                <button type="submit"></button>
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
             </form>
         </nav>
     )
