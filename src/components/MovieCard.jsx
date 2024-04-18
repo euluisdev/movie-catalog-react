625c60fc5f51fae8b846b72e05faee5ecffdaa9c
euluis-dev <fluisf00@gmail.com>
2024-04-17 21:12:53 -0300

    the MovieCard components were created to handle movie information

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+import { FaStar } from "react-icons/fa";
+
+const MovieCard = () => {
+    return (
+        <div>MovieCard</div>
+    )
+}
+
+export default MovieCard;
