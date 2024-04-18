df52a2f31d78812ad627a634b55f937f8f4be58c
euluis-dev <fluisf00@gmail.com>
2024-04-18 08:47:10 -0300

    the structure was crested to print movie cards in MovieCards components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,20 @@ import { Link } from "react-router-dom";
 
 import { FaStar } from "react-icons/fa";
 
-const MovieCard = () => {
+const imageUrl = import.meta.env.VITE_IMG;
+
+const MovieCard = ({ movie, showLink = true }) => {
     return (
-        <div>MovieCard</div>
+        <div className="movieCard">
+            <img src={imageUrl + movie.poster_path} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p>
+                <FaStar /> {movie.vote_average}
+            </p>
+            {
+                showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>
+            }
+        </div>
     )
 }
 
