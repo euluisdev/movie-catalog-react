db228c4ef8446f5fe445b7eafa4f4a103c02a54b
euluis-dev <fluisf00@gmail.com>
2024-04-19 11:58:37 -0300

    the state was created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import "./NavBar.css";
 
 const NavBar = () => {
+    const [search, setSearch] = useState('');
 
     return (
         <nav id="navbar">
@@ -13,7 +15,12 @@ const NavBar = () => {
                 </Link>
             </h2>
             <form>
-                <input type="text" placeholder="Busque seu filme" />
+                <input 
+                    type="text" 
+                    placeholder="Busque seu filme" 
+                    onChange={(e) => setSearch(e.target.value)} 
+                    value={search}
+                />
                 <button type="submit">
                     <BiSearchAlt2 />
                 </button>
