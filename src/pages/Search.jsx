45eeab318c4c188676ad92d89cd9ff731eb9a10f
euluis-dev <fluisf00@gmail.com>
2024-04-19 13:00:51 -0300

    added .env file and .css file

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import './MoviesGrid.css';
+
 const Search = () => {
     return (
         <div>
