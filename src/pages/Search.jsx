df5e77c78cf3cb310ed8821152c8f85f61ddae9a
euluis-dev <fluisf00@gmail.com>
2024-04-19 12:56:50 -0300

    imports were made to thr Search.jsx page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
 const Search = () => {
     return (
         <div>
