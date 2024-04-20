16e861b26f354d8e7835636afa5583f6168f2ea4
euluis-dev <fluisf00@gmail.com>
2024-04-20 20:36:11 -0300

    the necessary imports were made for the page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,17 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+    bsGraphUp, 
+    BaWallet2, 
+    BsHourglassSplit, 
+    BsFillFileEarmarkTextFill
+} from 'react-icons/bs';
+
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API  // import variable of the .env
+const apiKey = import.meta.env.VITE_API_KEY  // import variable of the .env
+
 const Movie = () => {
     return (
         <div>
